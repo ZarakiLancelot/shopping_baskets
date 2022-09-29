942a238716f0963be65327f64b3fe84fe8c620ab
ZarakiLancelot <kenpachi2783@gmail.com>
2022-09-29 05:17:44 -0600

    calculations and print output

--- product.rb
+++ product.rb
@@ -1,20 +1,38 @@
-require 'constants'
+require_relative 'constants'
 
 class Product
-	attr_accessor :name, :price, :quantity, :productType, :sales_item
+	attr_accessor :name, :price, :quantity, :exempt, :imported, :tax
 
-	def initialize(name, price, quantity, productType)
+	def initialize(name, price, quantity, exempt, imported)
 		@name = name
-		@price = price
-		@quantity = quantity
-		@productType = productType
+		@price = price.to_f
+		@quantity = quantity.to_i
+		@exempt = exempt
+		@imported = imported
+		@tax = 0.0
 	end
 
 	def imported?
-		productType == Constants::PRODUCT_TYPES[3]
+		@imported == true
 	end
 
 	def exempt?
-		productType == Constants::PRODUCT_TYPES[2]
+		@exempt == true
+	end
+
+	def calculate_tax
+		if imported? && !exempt?
+			@tax = ((@price * Constants::IMPORT_TAX_RATE) + (@price * Constants::SALES_TAX_RATE))
+		elsif imported? && exempt?
+			@tax = (@price * Constants::IMPORT_TAX_RATE)
+		elsif !imported? && !exempt?
+			@tax = (@price * Constants::SALES_TAX_RATE)
+		else
+			@tax = 0.0
+		end
+	end
+
+	def rounding_nearest(value)
+		((Constants::ROUNDING_COEFFICIENT * value) / Constants::ROUNDING_COEFFICIENT).round(2)
 	end
 end
